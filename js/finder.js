c0030c106dfba60cfb3e562b68568cb0d239ba39
KrehyDev <93307379+KrehyDev@users.noreply.github.com>
2022-12-27 02:08:41 +0100

    Create finder.js

---
+++ js/finder.js
@@ -0,0 +1,16 @@
+//script který dělá to že se odkazy z menu otevírají na této stránce a ne na nové
+
+  document.querySelectorAll('nav a').forEach(function(odkaz) {
+    odkaz.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById('content').innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", this.href, true);
+      xhttp.send();
+    });
+  });
