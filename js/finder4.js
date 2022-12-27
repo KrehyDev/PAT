ba0dd02bf46ef0b5c5184acd25e3aac6cced18a8
KrehyDev <93307379+KrehyDev@users.noreply.github.com>
2022-12-27 02:07:41 +0100

    Update finder4.js

--- js/finder4.js
+++ js/finder4.js
@@ -1,47 +1,17 @@
-const prekladac = "https://files.smarty.cz/pat/tools/translator/final/translator-v2.0.html"
+//script který dělá to že se odkazy z menu otevírají na této stránce a ne na nové
 
-document.getElementById("prekladac").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", prekladac);
-});
+  document.querySelectorAll('nav a').forEach(function(odkaz) {
+    odkaz.addEventListener('click', function(event) {
+      event.preventDefault();
 
-const watermark = "https://files.smarty.cz/pat/tools/tool-watermark.html"
-
-document.getElementById("watermark").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", watermark);
-});
-
-const vystava = "https://files.smarty.cz/pat/dox/dox-vystava.html"
-
-document.getElementById("vystava").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", vystava);
-});
-
-const format = "https://files.smarty.cz/pat/dox/dox-format.html"
-
-document.getElementById("format").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", format);
-});
-
-const hesla = "https://files.smarty.cz/pat/dox/dox-hesla.html"
-
-document.getElementById("hesla").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", hesla);
-});
-
-const filezilla = "https://files.smarty.cz/pat/dox/dox-filezilla.html"
-
-document.getElementById("filezilla").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", filezilla);
-});
-
-const xnconvert = "https://files.smarty.cz/pat/dox/dox-xnconvert.html"
-
-document.getElementById("xncovert").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", xnconvert);
-});
-
-const bagr = "https://files.smarty.cz/pat/dox/dox-bagr.html"
-
-document.getElementById("bagr").addEventListener("click", function() {
-  document.getElementById("finder").setAttribute("data", bagr);
-});
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById('content').innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", this.href, true);
+      xhttp.send();
+    });
+  });
+  
